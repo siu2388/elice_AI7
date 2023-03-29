2b4242a48fabcd99f217d4e63bb3b0be61c0cd0b
siu2388 <swear428@naver.com>
2023-03-29 14:11:11 +0900

    useeffect 사용

--- 05-react1/0329react/src/App.js
+++ 05-react1/0329react/src/App.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import counterStyle from "./Counter.module.css";
-console.log("counterStyle", counterStyle);
+// console.log("counterStyle", counterStyle);
 
 function Counter({ title, initValue }) {
   const [count, setCount] = useState(initValue);
   console.log("Counter", count);
+
+  //서버에서 데이터 가져오기
+  //npx json-server --port 9999 --watch db.json
+  //useEffect(첫번째자리함수,[빈배열]) -컴포넌트 렌더링할 때 딱 한번만 실행이된다 -> 빈 배열 // 빈 배열 안주면 렌더링 될때마다 계속 실행된다.
+  //서버통신, 파일저장, 언제끝날지 모르는 작업을 useEffect 훅안에 넣어주면 된다.
+
+  useEffect(() => {
+    fetch("http://localhost:9999/counter")
+      .then((res) => {
+        return res.json();
+      })
+      .then((result) => {
+        setCount(result.value);
+      });
+  }, []);
+
   function up() {
     setCount(count + 1);
     console.log("up", count);
@@ -33,7 +49,7 @@ function Counter({ title, initValue }) {
 function App() {
   return (
     <div className="App">
-      <Counter title="카운터" initValue={10}></Counter>
+      <Counter title="카운터" initValue={0}></Counter>
     </div>
   );
 }
