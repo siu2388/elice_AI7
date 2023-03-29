d96ffdaa06c1511276aae8d17c5fe76348b57829
siu2388 <swear428@naver.com>
2023-03-29 14:43:08 +0900

    fetch로 count변수 받아와서 리렌더링 완성

--- 05-react1/0329react/src/App.js
+++ 05-react1/0329react/src/App.js
@@ -1,51 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import counterStyle from "./Counter.module.css";
-// console.log("counterStyle", counterStyle);
-
-function Counter({ title, initValue }) {
-  const [count, setCount] = useState(initValue);
-  console.log("Counter", count);
-
-  //서버에서 데이터 가져오기
-  //npx json-server --port 9999 --watch db.json
-  //useEffect(첫번째자리함수,[빈배열]) -컴포넌트 렌더링할 때 딱 한번만 실행이된다 -> 빈 배열 // 빈 배열 안주면 렌더링 될때마다 계속 실행된다.
-  //서버통신, 파일저장, 언제끝날지 모르는 작업을 useEffect 훅안에 넣어주면 된다.
-
-  useEffect(() => {
-    fetch("http://localhost:9999/counter")
-      .then((res) => {
-        return res.json();
-      })
-      .then((result) => {
-        setCount(result.value);
-      });
-  }, []);
-
-  function up() {
-    setCount(count + 1);
-    console.log("up", count);
-    // setCount((oldCount) => {
-    //   console.log("oldCount", oldCount);
-    //   return oldCount + 1;
-    // });
-  }
-  // //react css 입히는 법 div={design} 사용
-  const design = {
-    border: "5px solid red",
-    backgroundColor: "lightgray",
-  };
-  return (
-    <div style={design}>
-      <h1 className="title">{title}</h1>
-      <button onClick={up} className={counterStyle.rightSpace}>
-        +
-      </button>{" "}
-      {count}
-    </div>
-  );
-}
-
+import { Counter } from "./Counter";
 function App() {
   return (
     <div className="App">
