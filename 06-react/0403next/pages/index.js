abd5c49b29b727000120865688b0c94b48c9a39c
siu2388 <swear428@naver.com>
2023-04-03 11:51:00 +0900

    전반전 끝

--- 06-react/0403next/pages/index.js
+++ 06-react/0403next/pages/index.js
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <>
-      <h1></h1>
+      <h1><a href="/">WEB</a></h1>
+      <ol>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+      </ol>
+      <article>
+        <h2>Welcome</h2>
+      </article>
     </>
   )
 }
