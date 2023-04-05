e19454b1c94dbaa49f748b744b0174f7cee622e8
siu2388 <swear428@naver.com>
2023-04-05 10:47:50 +0900

    0405 1교시 [id].js useEffect로 변경하기

--- 06-react/0403next/pages/read/[id].js
+++ 06-react/0403next/pages/read/[id].js
@@ -1,13 +1,27 @@
 import { useRouter } from "next/router";
 import Layout from "../../components/layout";
+import { useEffect, useState } from "react";
 
 export default function Read() {
   const router = useRouter();
+  const [topic, setTopic] = useState({ title: "", body: "" });
   console.log("router", router.query.id); //id값 가져오기
+  //useEffect(파라미터 함수,언제실행될까)
+  //fetch
+  useEffect(() => {
+    if (router.query.id !== undefined) {
+      fetch("http://localhost:9999/topics/" + router.query.id)
+        .then((res) => res.json())
+        .then((data) => {
+          setTopic(data);
+          console.log("data", data);
+        });
+    }
+  }, [router.query.id]);
   return (
     <>
-      <h2>Read</h2>
-      Hello, Read{router.query.id}
+      <h2>{topic.title}</h2>
+      {topic.body}
     </>
   );
 }
